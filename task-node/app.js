1800dfe65044a9112d9530eea6cb1fc12e059d48
RonniePlay <reveinoff@gmail.com>
2025-06-17 12:43:06 +0300

    Update app file

--- task-node/app.js
+++ task-node/app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
 app.use("/api/auth", authRoutes);
 
 module.exports = app;
